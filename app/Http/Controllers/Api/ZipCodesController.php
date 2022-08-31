f27988310a7d49c3202f91d1e1f2b0b9b05f26aa
John Fredy Velasco Bareño <jvelasco@awake.travel>
2022-08-30 19:47:31 -0500

    Endpoint de consulta.

---
+++ app/Http/Controllers/Api/ZipCodesController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\ZipCode;
+use Illuminate\Http\Request;
+
+class ZipCodesController extends Controller
+{
+    public function getZipCode(int $zipCode, Request $request)
+    {
+        $dataZipCode = ZipCode::with(['federal_entity:key,name,code,id', 'settlements.settlement_type', 'municipality:key,name,id'])
+            ->whereZipCode($zipCode)
+            ->select(['zip_code', 'locality', 'federal_entity_id', 'id', 'municipality_id'])
+            ->firstOrFail();
+
+        return response()
+            ->json(
+                [
+                    'zip_code' => $dataZipCode->zip_code,
+                    'locality' => $dataZipCode->locality,
+                    'federal_entity' => [
+                        'key' => $dataZipCode->federal_entity->key,
+                        'name' => $dataZipCode->federal_entity->name,
+                        'code' => $dataZipCode->federal_entity->code,
+                    ],
+                    'settlements' => $dataZipCode->settlements->map(function ($settlement) {
+                        return [
+                            'key' => $settlement->key,
+                            'name' => $settlement->name,
+                            'zone_type' => $settlement->zone_type,
+                            'settlement_type' => [
+                                'name' => $settlement->settlement_type->name,
+                            ],
+                        ];
+                    }),
+                    'municipality' => [
+                        'key' => $dataZipCode->municipality->key,
+                        'name' => $dataZipCode->municipality->name,
+                    ],
+                ]
+            );
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\ZipCodesController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get('/zip-codes/{zipCode}', [ZipCodesController::class,'getZipCode'])
+    ->name("api.zip-codes")
+    ->where([
+        'zipCode' => '^[0-9]{1,5}$'
+    ]);
